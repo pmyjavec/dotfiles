1dbbe54bda828432d179bceef5753c0b1253835b
Pauly Myjavec <pauly@buzbox.net>
2012-09-23 19:14:00 +1000

    Manages a few more files, some fixes

--- install.sh
+++ install.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
+# Sets up everything the way I like it...
+
+cd `dirname $0`
+echo `pwd`
+
+echo "Installing/updating Vundle"
+which git 1>&2> /dev/null && (if [ ! -d ~/.vim/bundle ] ; then git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle; fi )\
+        || (echo Install Git first...; exit 1)
+ 
+echo Updating RC files...
 git pull
+
+echo "Install files...existing files will be moved to /tmp"
 for i in `ls .`; do 
   if [ "$i" != "install.sh" ] 
    then
-    mv -v ~/.$i /tmp/
+    mv -v ~/.$i /tmp/$i.bak
     ln -vs `pwd`/$i ~/.$i
   fi 
 done
+
+# Sets up gem home etc from bash_profile, installs gems
+source ~/.bash_profile
+echo "Installing my favourite gems..."
+which bundle 1>&2> /dev/null && bundle install || echo "Check your ruby env...bundler missing"
+echo done
--- vimrc
+++ vimrc
@@ -1,17 +1,23 @@
-syntax on
-set list
-set t_Co=256
-colorscheme vividchalk
-set ts=2
-set expandtab
-set nocompatible
+" Install extras first
+set nocompatible               " be iMproved
+filetype off                   " required!
+
 set rtp+=~/.vim/bundle/vundle/
 call vundle#rc()
-
-map g i<CR><Esc>h
 
 Bundle 'gmarik/vundle'
 Bundle 'sukima/xmledit'
 Bundle 'vim-ruby/vim-ruby'
+Bundle 'vividchalk'
 
+call vundle#rc()
+
+" Set basic options
+set list
+set t_Co=256
+set ts=2
+set expandtab
+syntax on
 filetype plugin indent on
+colorscheme vividchalk
+
