810ca779ea8691c81039ce8da980a0b1bf4707a9
Paul Myjavec <pauly@buzbox.net>
2016-05-06 18:20:19 +0200

    Don't install Brewfile

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-
 for fn in *; do
-  if [ ! $fn == "README.md" -a ! $fn == "install.sh" ]; then
+  if [ ! $fn == "README.md" -a ! $fn == "install.sh" -a ! $fn == "Brewfile" ]; then
 
     target="$HOME/.$fn"
 
